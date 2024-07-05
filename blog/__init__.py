a1b92af8898d87b414cd01cc187e6d80970e16bc
sean <shanaesenderlingr@gmail.com>
2024-07-05 08:34:23 +0800

    init route and base.html

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,6 +6,13 @@ def create_app():
     # SECRET_KEY 的作用?
     app.config["SECRET_KEY"] = "salkdjkalsjdlakq310948uc89"
 
+    from .views import views
+    from .auth import auth
+
+
+    # url_prefix: 就是這些路徑的前綴
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
 
     return app
 
